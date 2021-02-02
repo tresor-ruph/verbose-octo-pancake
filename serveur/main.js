d2208c5351c6c1344365834b7038d7e996ca0a93
tresor-ruph <tresortek8@gmail.com>
2021-02-02 19:44:39 +0100

    env variables

--- serveur/main.js
+++ serveur/main.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const dotenv = require('dotenv')
+
+dotenv.config();
 
 app.use(express.static(path.join(__dirname,'build')));
 
@@ -12,8 +15,8 @@ app.get("/api/test", (req, res)=> {
     return res.send("hello world !");
 })
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log('the server is running');
+app.listen(process.env.PORT, () => {
+    console.log(`your server is running on port ${process.env.PORT}`);
 });
 
 module.exports = app;
