4e7df1fde107a68dbcb26638202149a3e138ce90
tresor-ruph <tresortek8@gmail.com>
2021-02-02 20:19:22 +0100

    eslint nodejs

--- serveur/main.js
+++ serveur/main.js
@@ -1,22 +1,22 @@
 const express = require('express');
+
 const app = express();
+
 const path = require('path');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 
 dotenv.config();
 
-app.use(express.static(path.join(__dirname,'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.get("/api/test", (req, res)=> {
-    return res.send("hello world !");
-})
+app.get('/api/test', (req, res) => res.send('hello world !'));
 
 app.listen(process.env.PORT, () => {
-    console.log(`your server is running on port ${process.env.PORT}`);
+  console.log(`your server is running on port ${process.env.PORT}`);
 });
 
 module.exports = app;
