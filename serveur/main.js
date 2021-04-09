52c1ae682f92bb4dca72893dd33e6998f4c6ce78
tresor-ruph <tresortek8@gmail.com>
2021-04-09 20:17:47 +0200

    debug prod

--- serveur/main.js
+++ serveur/main.js
@@ -17,9 +17,9 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 5
 const apiRoutes = require('./routes')(app)
 
 //*************************************************************** */
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client')));
 app.get(['/','*'], (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
 
 
