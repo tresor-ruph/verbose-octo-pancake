693d022fa81a36e9c03fc966e9dc6a5b5c94393f
tresor-ruph <tresortek8@gmail.com>
2021-04-09 20:25:25 +0200

    debug prod

--- serveur/main.js
+++ serveur/main.js
@@ -14,13 +14,15 @@ dotenv.config();
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
-const apiRoutes = require('./routes')(app)
 
 //*************************************************************** */
 app.use(express.static(path.join(__dirname, 'client')));
 app.get(['/','*'], (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
+
+const apiRoutes = require('./routes')(app)
+
 
 
 
