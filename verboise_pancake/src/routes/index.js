7bf2ddbe1eeffdb11a9d2d06934e2dc86a503442
tresor-ruph <tresortek8@gmail.com>
2021-04-09 02:29:15 +0200

    debug v1_3

--- verboise_pancake/src/routes/index.js
+++ verboise_pancake/src/routes/index.js
@@ -1,5 +1,5 @@
 
-import React, { useEffect } from "react";
+import React from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "assets/css/animate.min.css";
