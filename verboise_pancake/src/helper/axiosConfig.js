9382ace24095ad23e17cf956cf3597aa3a668732
tresor-ruph <tresortek8@gmail.com>
2021-04-09 22:13:52 +0200

    deploy front end

--- verboise_pancake/src/helper/axiosConfig.js
+++ verboise_pancake/src/helper/axiosConfig.js
@@ -4,9 +4,5 @@ import ls from 'local-storage'
 // axios.defaults.baseURL='http://localhost:8000/api'
 axios.defaults.baseURL='https://verbose-octo-pancake.herokuapp.com/api'
 
-
-
-
-
 const token =  ls.get('token') 
 axios.defaults.headers.common['Authorization'] = token != undefined ? `Bearer ${token}` : '' 
