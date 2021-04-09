4a70c0c9afd7a2da07f984c2cd7d58e7274a4d06
tresor-ruph <tresortek8@gmail.com>
2021-04-09 21:33:05 +0200

    debug prod

--- serveur/main.js
+++ serveur/main.js
@@ -20,7 +20,7 @@ const apiRoutes = require('./routes')(app)
 
 
 app.use(express.static(path.join(__dirname, 'client')));
-app.get(['/','./','*'], (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
 
