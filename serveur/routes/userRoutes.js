3bcb287a1941ff35ad66927cda4daf6187742a1d
tresor-ruph <tresortek8@gmail.com>
2021-05-23 04:24:36 +0200

    settings

--- serveur/routes/userRoutes.js
+++ serveur/routes/userRoutes.js
@@ -6,6 +6,7 @@ module.exports = (app) => {
     app.get('/api/user/:username', userController.getUser)
     app.delete('/api/user/:username', userController.deleteUser)
     app.put('/api/user', userController.updateUser)
+    app.put('/api/user', userController.updatePassword)
     app.get('/api/Login/:user', userController.userLogin)
     app.post('/api/Signin', userController.createUser)
     app.get('/api/confirmEmail/:id', userController.confirmEmail)
